cece21b01a3420a8abaa94977222e07dbedabefd
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-11-09 07:14:42 -0800

    added font to project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { BrowserRouter, Routes } from "react-router-dom";
 import { config } from "./routes/config";
 
 export default function App() {
+
+  const link = document.createElement('link');
+  link.href = 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap';
+  link.rel = 'stylesheet';
+  document.head.appendChild(link);
+  
   return (
     <BrowserRouter>
       <Routes>{config}</Routes>
