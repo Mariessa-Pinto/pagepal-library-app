71c0bca863edcd60007f4ded29136630cbe2e1db
anikagrewal <a.grewal.work@gmail.com>
2023-11-12 03:04:59 -0800

    home page done

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,44 @@
 import { Link } from "react-router-dom";
 import { Container, Box, Button } from "@mui/material";
 
+import HomeBooks from '../assets/homePageBooks.svg'
+
 export default function Home() {
     return (
-        <Container>
-            <p>
-                <Link to="/library">Library</Link>
-            </p>
+        <Container style={{display: 'flex', flexDirection: 'row'}}>
+            <Box
+            sx={{ display: 'flex', flexDirection: 'column' }}>
+            <h1 style={{ color: 'black', fontSize: 96, fontWeight: 'bold', marginTop: '10rem' }}>Page Pal</h1>
+            <p style={{ color: 'black', fontSize: 20, fontWeight: 'medium', width: '25em'}}>Welcome to PagePal, your digital haven for book lovers and avid readers alike. Just like the cozy shelves of a physical library, our platform offers a virtual sanctuary where you can organize, explore, and cherish your literary treasures. With the power of Google API integration, you can effortlessly add and remove books, create custom bookshelves, and embark on a journey to curate your own digital library. Whether you're a dedicated bookworm, a casual reader, or a collector of knowledge, PagePal is here to make managing your personal library an enjoyable and seamless experience.</p>
+            <Link
+                    to="/library"
+                    style={{ textDecoration: 'none' }}>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            width: '270.19px',
+                            height: '50px',
+                            backgroundColor: '#593122',
+                            marginRight: '105px',
+                            '&:hover, &.Mui-focusVisible': {
+                                backgroundColor: '#A6633C',},
+                            marginTop: '3rem'
+                        }}
+                    >
+                    Your Library
+                    </Button>
+                </Link>
+            </Box>
+            
+           
+                <img
+                    src={HomeBooks}
+                    alt='books on shelf'
+                    height={1180}
+                    style={{ marginLeft: '11rem', width: '100rem' }}
+                />
+            
+           
         </Container>
     );
 }
